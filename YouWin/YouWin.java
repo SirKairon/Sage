38b7eeb87f74517cc01e96c5fd137dc42816a9de
SirKairon <59523526+SirKairon@users.noreply.github.com>
2022-10-19 11:48:29 -0600

    Add files via upload

---
+++ YouWin/YouWin.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class YouWin here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class YouWin extends Actor
+{
+    /**
+     * Act - do whatever the YouWin wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    private static GreenfootImage img1 = new GreenfootImage("youwin.png");
+    public void act() 
+    {
+        
+        // Add your action code here.
+    }
+    
+    
+   
+}
